87daa45949833b03e5ba797fa7ffb88b49c0ff51
abhishekverma22 <vermaabhishekabhi22@gmail.com>
2025-10-25 16:56:18 +0530

    Home Page complete

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import HeroSection from "../components/HeroSection";
+import LiveLocationWeather from "../components/LiveLocationWeather";
+import Description from "../components/Description";
+import Footer from "../components/Footer";
 
 const Home = () => {
   return (
-    <div className="min-h-screen bg-gray-500">
+    <div className="min-h-screen ">
       <Navbar />
+      <HeroSection />
+      <LiveLocationWeather/>
+      <Description/>
+      <Footer/>
     </div>
   );
 };
